f1ae633d7d3f3ad281934462a4e23284f1d58ea6
azad-source <azad.63.mamedov@gmail.com>
2024-11-22 02:20:59 +0400

    refactoring

--- src/components/FileUploaderCard/FileUploaderCard.tsx
+++ src/components/FileUploaderCard/FileUploaderCard.tsx
@@ -1,32 +1,12 @@
 import styles from './FileUploaderCard.module.scss';
 import React from 'react';
-import {  FileUploader,  FileUploaderAttachedFile } from '@skbkontur/react-ui';
+import { FileUploader, FileUploaderProps } from '@skbkontur/react-ui';
 
-interface IProps {
-  isSizeError?: boolean;
-  disabled?: boolean;
-  accept?: string;
+interface IProps extends FileUploaderProps {
   cardSize?: number;
-  onAttache: (files: FileUploaderAttachedFile[]) => void;
 }
 
-export const FileUploaderCard: React.FC<IProps> = ({
-  // isSizeError = false,
-  disabled = false,
-  accept,
-  cardSize = 140,
-  onAttache,
-}) => {
-
-  return (
-    <FileUploader
-      multiple
-      hideFiles
-      accept={accept}
-      disabled={disabled}
-      style={{ width: cardSize, height: cardSize }}
-      className={styles.root}
-      onAttach={onAttache}
-    />
-  );
+export const FileUploaderCard: React.FC<IProps> = ({ cardSize = 140, ...props }) => {
+  const uploaderStyles: React.CSSProperties = { width: cardSize, height: cardSize };
+  return <FileUploader {...props} style={uploaderStyles} className={styles.root} />;
 };
